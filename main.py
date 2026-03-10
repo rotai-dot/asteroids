8d0ce10b9b20cec88393702a555fa70501ab818a
rotai-dot <rotai2@gmail.com>
2026-03-10 14:08:50 +0100

    first working state, logging introduced

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import pygame 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
 def main():
-    print("Hello from asteroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
